d059e5f81966a0aa76ee933866536cce9d0d9c0c
nohsangwoo <nsgr12@naver.com>
2022-12-23 14:06:17 +0900

    complete

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Android fundamentals 02.3: Implicit intents
+
+- ref: https://stackoverflow.com/questions/62535856/intent-resolveactivity-returns-null-in-api-30
--- app/src/main/java/com/example/noh/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/noh/implicitintents/MainActivity.java
@@ -1,14 +1,84 @@
 package com.example.noh.implicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
+import android.content.Intent;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = "LOG_TAG";
+//    final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+
+    private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
+    private EditText mShareTextEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void openWebsite(View view) {
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+
+        // Get the URL text.
+        String url = mWebsiteEditText.getText().toString();
+
+        // Parse the URI and create the intent.
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Find an activity to hand the intent and start that activity.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+    public void openLocation(View view) {
+        mLocationEditText = findViewById(R.id.location_edittext);
+
+        // Get the string indicating a location. Input is not validated; it is
+        // passed to the location handler intact.
+        String loc = mLocationEditText.getText().toString();
+
+        // Parse the location and create the intent.
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        // Find an activity to handle the intent, and start that activity.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this intent!");
+        }
+    }
+
+
+    public void shareText(View view) {
+        mShareTextEditText = findViewById(R.id.share_edittext);
+
+        Log.d(LOG_TAG, "shareText: ");
+        String txt = mShareTextEditText.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder
+                .from(this)
+                .setType(mimeType)
+                .setChooserTitle(R.string.share_text_with)
+                .setText(txt)
+                .startChooser();
     }
 }
